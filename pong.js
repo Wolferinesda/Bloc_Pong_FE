8e998765e843fad40edbb4ab7c274cfb27a1d218
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-05-14 21:36:30 -0400

    Paddle moves with the 'o' and 'p' keys

--- pong.js
+++ pong.js
@@ -6,17 +6,27 @@ c.fillStyle = 'black';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 // paddles
-function Paddle(x,y) {
+function Paddle(x, y, width, height) {
   this.x = x;
   this.y = y;
+  this.height = height;
+  this.width = width;
+  this.speed = 10;
+
+  this.move = function(dy) {
+    if(this.y + dy > 0 && this.y + this.height + dy < canvas.height) {
+    this.y += dy;
+    }
+  };
+
   this.render = function() {
     c.fillStyle = 'white';
-    c.fillRect(this.x, this.y, 10, 100);
+    c.fillRect(this.x, this.y, this.width, this.height);
   };
 }
 
 // the ball
-function Ball(x,y) {
+function Ball(x, y) {
   this.x = x;
   this.y = y;
   this.render = function(){
@@ -27,9 +37,35 @@ function Ball(x,y) {
   };
 }
 
-var player = new Paddle(50, 100);
-var computer = new Paddle(550, 110);
+var player = new Paddle(50, 100, 10, 100);
+var computer = new Paddle(550, 110, 10, 100);
 var ball = new Ball(500, 250);
+
+function addKeyEvent() {
+  window.addEventListener('keydown', keyPress, true);
+}
+
+function keyPress(direction) {
+  switch (direction.keyCode) {
+    case 80:
+      player.move(-player.speed);  // up
+      break;
+    case 79:
+      player.move(player.speed); // down
+      break;
+  }
+}
+
+var animate = window.requestAnimationFrame || function(callback) { window.setTimeout(callback, 1000/60) };
+
+var step = function() {
+  c.clearRect(0,0,canvas.width,canvas.height);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  render();
+  animate(step);
+};
 
 var render = function(){
   player.render();
@@ -38,5 +74,6 @@ var render = function(){
 };
 
 window.onload = function(){
-  render();
+  addKeyEvent();
+  step();
 };
